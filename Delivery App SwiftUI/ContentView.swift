a713a6842192665119f3874b97a17dfab5c9cc5e
OkonovUrmat <okonov.okonov@gmail.com>
2022-01-08 19:35:10 +0600

    Create Search bar and filter

--- Delivery App SwiftUI/ContentView.swift
+++ Delivery App SwiftUI/ContentView.swift
@@ -6,75 +6,76 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @State private var search: String = ""
+    
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
+        VStack(alignment: .leading, spacing: 16) {
+            HomeNavBarView()
+            
+            Text("Hello, friend")
+                .font(.caption)
+                .fontWeight(.medium)
+                .foregroundColor(.gray)
+            
+            Text("Do ya'll wanna cook it up?")
+                .font(.title)
+                .fontWeight(.bold)
+            
+            SearchBar(search: $search)
+            
+            Spacer()
+        }.padding()
+            
     }
+}
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
 
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
+}
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+struct HomeNavBarView: View {
+    var body: some View {
+        HStack {
+            Image("menu")
+                .onTapGesture {
+                    
+                }
+            
+            Spacer()
+            
+            Image("notifications")
+                .onTapGesture {
+                    
+                }
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+struct SearchBar: View {
+    @Binding var search: String
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            HStack {
+                Image("search")
+                TextField("Search for Recipes", text: $search)
+            }.padding()
+                .background(Color(UIColor.lightGray))
+                .cornerRadius(8.0)
+            
+            Image("filter")
+                .padding()
+                .background(Color(UIColor.lightGray))
+                .cornerRadius(8.0)
+                .onTapGesture {
+                    
+                }
+        }
     }
 }
